80ef7366439ef2ff3473c3d4eca5a8212ffe08c4
lori33c <lcha2@uci.edu>
2023-03-10 18:10:42 -0800

    got

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
-	// import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<!-- <Header /> -->
 
 	<main>
 		<slot />
@@ -30,21 +28,6 @@
 		box-sizing: border-box;
 	}
 
-	/* footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	} */
-
 	@media (min-width: 480px) {
-		/* footer {
-			padding: 12px 0;
-		} */
 	}
 </style>
